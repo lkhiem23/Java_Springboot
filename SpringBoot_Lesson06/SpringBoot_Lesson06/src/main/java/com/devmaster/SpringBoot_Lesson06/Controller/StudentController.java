b9a2df4cbaf32e5a10aea6e199e975b6f64a6a63
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-01-04 09:13:51 +0700

    Update StudentController.java

--- SpringBoot_Lesson06/SpringBoot_Lesson06/src/main/java/com/devmaster/SpringBoot_Lesson06/Controller/StudentController.java
+++ SpringBoot_Lesson06/SpringBoot_Lesson06/src/main/java/com/devmaster/SpringBoot_Lesson06/Controller/StudentController.java
@@ -1,23 +1,60 @@
 package com.devmaster.SpringBoot_Lesson06.Controller;
 
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
+import org.springframework.ui.Model;
+import com.devmaster.SpringBoot_Lesson06.DTO.StudentDTO;
 import com.devmaster.SpringBoot_Lesson06.Entity.Student;
 import com.devmaster.SpringBoot_Lesson06.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
+// @RestController ---- Dùng API
 @RequestMapping("/students")
 public class StudentController {
     @Autowired
     private StudentService studentService;
-    
-    // Get All
+
+  /*
+  // Get All ------ API
     @GetMapping
     public List<Student> getAllStudents() {
         return studentService.findAll();
     }
+  */
+
+    @GetMapping
+    public String getAllStudents(Model model) {
+        List<Student> students = studentService.findAll();
+        model.addAttribute("students",students);
+        return "students/list";
+    }
+    //Get: Create
+    @GetMapping("/add-new")
+    public String addNewStudent(Model model) {
+        model.addAttribute("student",new StudentDTO());
+        return "students/create";
+    }
+
+
+    //Create Post
+    @PostMapping("/add-new")
+    public String saveNewStudent(@ModelAttribute("student") StudentDTO student) {
+        studentService.save(student);
+        return "redirect:/students";
+    }
+
+    //View
+    @GetMapping("/view/{id}")
+    public String viewStudent(@PathVariable(value="id") Long id, Model model) {
+        StudentDTO studentDTO = studentService.findById(id).orElseThrow(()-> new
+                IllegalIdentifierException("Invalid student id"+id));
+        model.addAttribute("student",studentDTO);
+        return "students/view";
+    }
+
+
 }
