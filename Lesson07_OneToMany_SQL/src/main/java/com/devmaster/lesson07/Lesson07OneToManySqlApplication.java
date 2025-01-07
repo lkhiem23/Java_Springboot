c9dbdf76c8a588bfdb1d53cdf421f02602c347be
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-01-07 18:18:07 +0700

    Add files via upload

---
+++ Lesson07_OneToMany_SQL/src/main/java/com/devmaster/lesson07/Lesson07OneToManySqlApplication.java
@@ -0,0 +1,13 @@
+package com.devmaster.lesson07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson07OneToManySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson07OneToManySqlApplication.class, args);
+	}
+
+}
