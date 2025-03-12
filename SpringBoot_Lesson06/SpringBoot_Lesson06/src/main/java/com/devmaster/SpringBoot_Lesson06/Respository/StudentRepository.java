ee08336cba1efc3e06548f111a2c9a9562a3c1dd
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-03-12 23:56:31 +0700

    Update StudentRepository.java

--- SpringBoot_Lesson06/SpringBoot_Lesson06/src/main/java/com/devmaster/SpringBoot_Lesson06/Respository/StudentRepository.java
+++ SpringBoot_Lesson06/SpringBoot_Lesson06/src/main/java/com/devmaster/SpringBoot_Lesson06/Respository/StudentRepository.java
@@ -3,6 +3,4 @@ package com.devmaster.SpringBoot_Lesson06.Respository;
 import com.devmaster.SpringBoot_Lesson06.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Long /*Tương ứng khóa trong Entity*/> {
-
-}
+public interface StudentRepository extends JpaRepository<Student, Long /*Tương ứng khóa trong Entity*/> {}
