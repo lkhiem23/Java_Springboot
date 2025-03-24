4c904080f6a110fb611ee77fcdf169a504462d33
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2025-03-24 23:59:18 +0700

    Update Info.java

--- lesson05/src/main/java/com/devmaster/lesson05/entity/Info.java
+++ lesson05/src/main/java/com/devmaster/lesson05/entity/Info.java
@@ -5,7 +5,6 @@ import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
-
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 
@@ -16,51 +15,4 @@ public class Info {
     String phone;
     String address;
 
-    public Info(String address, String email, String name, String phone, String website) {
-        this.address = address;
-        this.email = email;
-        this.name = name;
-        this.phone = phone;
-        this.website = website;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
 }
